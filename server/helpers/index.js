6ce0796d1341eeb8490fa0196b4209616fad15b5
Dima Pulkinen <dima@itsemast.com>
2022-09-20 13:20:46 +0000

    parsing of STRAPI_INIT_ADMIN environment variable

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -21,12 +21,18 @@ module.exports = {
     return superAdminRole;
   },
   initAdminData(env) {
+    // only parse STRAPI_INIT_ADMIN variable if it has 5 colon-separated parts
+    var config = [];
+    if (env.STRAPI_INIT_ADMIN && env.STRAPI_INIT_ADMIN.match(/.*:.*:.*:.*:.*/)) {
+      config = env.STRAPI_INIT_ADMIN.split(':');
+    }
+    // first priority: STRAPI_INIT_ADMIN, second priority: INIT_ADMIN_*, then fallback
     return {
-      username: env.INIT_ADMIN_USERNAME || '',
-      password: env.INIT_ADMIN_PASSWORD || 'admin',
-      firstname: env.INIT_ADMIN_FIRSTNAME || 'Admin',
-      lastname: env.INIT_ADMIN_LASTNAME || '',
-      email: env.INIT_ADMIN_EMAIL || 'admin@init-strapi-admin.strapi.io',
+      username: config[0] || (env.INIT_ADMIN_USERNAME || ''),
+      password: config[1] || (env.INIT_ADMIN_PASSWORD || 'admin'),
+      firstname: config[2] || (env.INIT_ADMIN_FIRSTNAME || 'Admin'),
+      lastname: config[3] || (env.INIT_ADMIN_LASTNAME || ''),
+      email: config[4] || (env.INIT_ADMIN_EMAIL || 'admin@init-strapi-admin.strapi.io'),
       blocked: false,
       isActive: true,
     }
